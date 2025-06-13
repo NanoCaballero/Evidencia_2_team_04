05e05588180458525323e4e7f5476e29430aea9b
NanoCaballero <A01722757@tec.mx>
2025-06-13 02:35:40 -0600

    arreglar 6

--- search.h
+++ search.h
@@ -1,4 +1,24 @@
 /*
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2013 SonarSource
+ * http://github.com/SonarOpenCommunity/sonar-cxx
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/*
  * Copyright (c) 2025 Team 4
  * SPDX-License-Identifier: MIT
  *
@@ -19,6 +39,7 @@
 #include <vector>
 #include <utility>
 
+// Function to find the closest central office to a new house location
 std::pair<int, double> find_closest_central(const std::pair<int, int>& newHouse, const std::vector<std::pair<int, int>>& centrals);
 
 #endif
