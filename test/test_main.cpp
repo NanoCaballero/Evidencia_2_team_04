1e14c5c5647a2e62c0d4bf6c27ca8d7d595a62d7
NanoCaballero <A01722757@tec.mx>
2025-06-13 00:20:40 -0600

    Primeros tests

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,53 +1,61 @@
-#include <gtest/gtest.h>
-#include "graph.h"
-#include "kruskal.h"
-#include "tsp.h"
-#include "ford_fulkerson.h"
-#include "search.h"
 
-TEST(KruskalTest, MST) {
+/*
+ * Archivo: test_main.cpp
+ * Descripción: Pruebas test
+ * Autores: 
+ * Bernardo Caballero Zambrano A01722757
+ * Juan Daniel Vázquez Alonso A01285354
+ * Harold Andres Lancheros Álvarez A00836416
+ * Francisco Vaquera
+ * Fecha: Junio 2025
+ */
+
+#include <gtest/gtest.h>
+#include "../kruskal.h"
+#include "../tsp.h"
+#include "../ford_fulkerson.h"
+#include "../search.h"
+
+// === Kruskal ===
+TEST(KruskalTest, BasicMST) {
     Graph g(4);
-    g.addEdge(0, 1, 10);
-    g.addEdge(0, 2, 6);
-    g.addEdge(0, 3, 5);
-    g.addEdge(1, 3, 15);
-    g.addEdge(2, 3, 4);
+    g.addEdge(0, 1, 1);
+    g.addEdge(1, 2, 2);
+    g.addEdge(2, 3, 3);
+    g.addEdge(0, 3, 4);
 
     auto mst = kruskalMST(g);
     EXPECT_EQ(mst.size(), 3);
 }
 
-TEST(TSPTest, Simple4Nodes) {
+// === TSP ===
+TEST(TSPTest, SimpleTour) {
     std::vector<std::vector<int>> dist = {
-        {0, 10, 15, 20},
-        {10, 0, 35, 25},
-        {15, 35, 0, 30},
-        {20, 25, 30, 0}
+        {0, 1, 2},
+        {1, 0, 3},
+        {2, 3, 0}
     };
     auto [path, cost] = solveTSP(dist);
+    EXPECT_EQ(cost, 6);
     EXPECT_EQ(path.front(), 'A');
     EXPECT_EQ(path.back(), 'A');
-    EXPECT_GT(cost, 0);
 }
 
-TEST(FordFulkersonTest, MaxFlow) {
-    std::vector<std::vector<int>> capacity = {
-        {0, 8, 0, 0, 3, 0},
-        {0, 0, 9, 0, 0, 0},
-        {0, 0, 0, 0, 7, 2},
-        {0, 0, 0, 0, 0, 5},
-        {0, 0, 7, 4, 0, 0},
-        {0, 0, 0, 0, 0, 0}
+// === Ford-Fulkerson ===
+TEST(FordFulkersonTest, SmallGraphFlow) {
+    std::vector<std::vector<int>> cap = {
+        {0, 3, 2, 0},
+        {0, 0, 5, 2},
+        {0, 0, 0, 3},
+        {0, 0, 0, 0}
     };
-    int maxFlow = fordFulkerson(capacity, 0, 5);
-    EXPECT_EQ(maxFlow, 5);
+    EXPECT_EQ(fordFulkerson(cap, 0, 3), 4);
 }
 
-TEST(SearchTest, ClosestCentral) {
-    std::vector<std::pair<int, int>> centrals = {
-        {0, 0}, {100, 100}, {200, 200}
-    };
-    auto [idx, dist] = findClosestCentral({90, 90}, centrals);
-    EXPECT_EQ(idx, 1);
-    EXPECT_GT(dist, 0);
+// === Closest Central ===
+TEST(SearchTest, FindsClosestCentral) {
+    std::vector<std::pair<int, int>> centrals = {{0, 0}, {4, 3}, {7, 1}};
+    std::pair<int, int> house = {1, 1};
+    auto [idx, dist] = findClosestCentral(house, centrals);
+    EXPECT_EQ(idx, 0);
 }
